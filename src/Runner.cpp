8fd4a62167a2e017d929627e55f780b50280bcab
Naautilus <matthewlyubchik@gmail.com>
2024-01-05 00:33:26 -0500

    no comment

--- src/Runner.cpp
+++ src/Runner.cpp
@@ -8,9 +8,9 @@ using namespace std;
 const double EXIT_RADIUS = 3.85*0.5;
 const double THROAT_RADIUS = 2.85*0.5;
 const double CHAMBER_RADIUS = 12.10*0.5;
-const double ATMOSPHERE_TO_EXIT_RADIUS_RATIO = 4; // cells between the chamber and atmosphere regions need to have matching faces which only integers and specific ratios work for
+const double ATMOSPHERE_TO_EXIT_RADIUS_RATIO = 5; // cells between the chamber and atmosphere regions need to have matching faces which only integers and specific ratios work for
 const double ATMOSPHERE_REGION_RADIUS = EXIT_RADIUS * ATMOSPHERE_TO_EXIT_RADIUS_RATIO;
-const double ATMOSPHERE_REGION_HEIGHT = 50;
+const double ATMOSPHERE_REGION_HEIGHT = 80;
 
 int main() {
 
@@ -31,7 +31,7 @@ int main() {
   vector<double> cellResolutionDistribution = {1, 1, 1}; // {1, 1, -more central- to +more outside+} - must be locked for the atmosphere and exit radius to be connected while having different sizes
 
   FileEditor::copyProjectToGeneratedDirectory();
-  ModelCreator::createModel(structureData, angularResolution, cellResolutionsPerUnit, cellResolutionDistribution);
+  ModelCreator::createModel(structureData, angularResolution, cellResolutionsPerUnit, cellResolutionDistribution); // 296 last run           // 1219s for 0.000296 on nOuterCorrectors = 1
   FileEditor::copyProjectToRunDirectory();
   SolverStarter::solve();
 
